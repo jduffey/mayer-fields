e995ffc2bd107de13f2414d99627cfe2d4e21e28
Jed Duffey <jedduffey@gmail.com>
2020-01-13 20:56:45 -0500

    update mayer range values

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 mayer_ranges = []
 [mayer_ranges.append(i) for i in range(600, 1501, 50)]
 [mayer_ranges.append(i) for i in range(300, 601, 10)]
-[mayer_ranges.append(i) for i in range(10, 301, 5)]
+[mayer_ranges.append(i) for i in range(50, 301, 5)]
+[mayer_ranges.append(i) for i in range(20, 50, 2)]
+[mayer_ranges.append(i) for i in range(1, 20, 1)]
