6ccc41bcf375a22a5b2a68f109715015907389dc
Jed Duffey <jedduffey@gmail.com>
2020-01-19 15:52:14 -0500

    add ETH variables

--- config.py
+++ config.py
@@ -8,8 +8,12 @@ mayer_ranges = []
 day_ratio_ranges = []
 [day_ratio_ranges.append(i) for i in range(1, 401)]
 
-price_data_csv = 'price_data.csv'
-mayer_values_csv = 'output-data/mayer_values.csv'
-day_ratios_csv = 'output-data/day_ratios.csv'
+BTC_price_data_csv = 'BTC_price_data.csv'
+BTC_mayer_values_csv = 'output-data/BTC_mayer_values.csv'
+BTC_day_ratios_csv = 'output-data/BTC_day_ratios.csv'
+BTC_currency_pair = 'BTC-USD'
 
-currency_pair = 'BTC-USD'
+ETH_price_data_csv = 'ETH_price_data.csv'
+ETH_mayer_values_csv = 'output-data/ETH_mayer_values.csv'
+ETH_day_ratios_csv = 'output-data/ETH_day_ratios.csv'
+ETH_currency_pair = 'ETH-USD'
