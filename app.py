b756fcc8c45e48eabf5b635dababfe20202d2b65
Jed Duffey <jedduffey@gmail.com>
2020-01-20 22:34:46 -0500

    refactor

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from coinbase.wallet.client import Client
 from utils import generate_mayer_values, generate_day_ratios,\
                   write_data_to_worksheet, get_yesterday,\
                   get_date_range, get_day_after
-from config import BTC_price_data_csv, BTC_mayer_values_csv,\
-                   BTC_day_ratios_csv, BTC_currency_pair,\
-                   ETH_price_data_csv, ETH_mayer_values_csv,\
-                   ETH_day_ratios_csv, ETH_currency_pair
+from config import coins_info
 
 coinbase_client = Client('<KEY_NOT_NEEDED>', '<SECRET_NOT_NEEDED>')
 
@@ -20,7 +17,7 @@ def get_most_recent_date(csv_filename):
         return list(csv.reader(f))[-1][0]
 
 def get_spot_price_for_date(currency_pair, date):
-    price_data = coinbase_client.get_spot_price(currency_pair=currency_pair, date = date)
+    price_data = coinbase_client.get_spot_price(currency_pair=currency_pair, date=date)
     return price_data['amount']
 
 def get_price_dict_for_dates(currency_pair, dates):
@@ -42,82 +39,35 @@ def remove_last_row_from_csv(csv_filename):
     df.drop(df.tail(1).index,inplace=True)
     df.to_csv(csv_filename, index=False)
 
+def update_price_data(price_data_csv, currency_pair, yesterday):
+    price_data_from_csv = import_csv_as_list(price_data_csv)
+    most_recent_date_in_price_data = price_data_from_csv[-1][0]
+    if most_recent_date_in_price_data == "NOW":
+        most_recent_date_in_price_data = price_data_from_csv[-2][0]
+        remove_last_row_from_csv(price_data_csv)
+    if most_recent_date_in_price_data < yesterday:
+        print(f'Most recent date in "{price_data_csv}": {most_recent_date_in_price_data}')
+        print('Gathering missing price data...\n')
+        missing_price_dates = get_date_range(get_day_after(most_recent_date_in_price_data), yesterday)
+        missing_price_data = get_price_dict_for_dates(currency_pair, missing_price_dates)
+        append_data_to_csv(price_data_csv, missing_price_data)
+        print(f'Updated "{price_data_csv}" with {len(missing_price_data)} record(s).\n')
+    else:
+        print(f'Most recent date in "{price_data_csv}" ({most_recent_date_in_price_data}) ' + \
+              f'is not before yesterday ({yesterday}); no data are missing.\n')
+    now_price = coinbase_client.get_spot_price(currency_pair=currency_pair)['amount']
+    print('Current: ${:,.2f}\n'.format(float(now_price)))
+    with open(price_data_csv,'a') as fd:
+        fd.write(f'NOW, {now_price}')
+
 
 if __name__ == '__main__':
 
-    print()
-    price_data_from_csv = import_csv_as_list(BTC_price_data_csv)
-    most_recent_date_in_price_data = price_data_from_csv[-1][0]
-
     yesterday = get_yesterday()
 
-    if most_recent_date_in_price_data == "NOW":
-        most_recent_date_in_price_data = price_data_from_csv[-2][0]
-        remove_last_row_from_csv(BTC_price_data_csv)
-
-    if most_recent_date_in_price_data < yesterday:
-
-        print(f'Most recent date in "{BTC_price_data_csv}": {most_recent_date_in_price_data}')
-        print('Gathering missing price data...\n')
-
-        missing_price_dates = get_date_range(get_day_after(most_recent_date_in_price_data), yesterday)
-
-        missing_price_data = get_price_dict_for_dates(BTC_currency_pair, missing_price_dates)
-
-        append_data_to_csv(BTC_price_data_csv, missing_price_data)
-        print(f'Updated "{BTC_price_data_csv}" with {len(missing_price_data)} record(s).\n')
-
-    else:
-        print(f'Most recent date in "{BTC_price_data_csv}" ({most_recent_date_in_price_data}) ' + \
-              f'is not before yesterday ({yesterday}); no data are missing.\n')
-
-    now_price = coinbase_client.get_spot_price(currency_pair=BTC_currency_pair)['amount']
-    print('BTC current: ${:,.2f}\n'.format(float(now_price)))
-    with open(BTC_price_data_csv,'a') as fd:
-        fd.write(f'NOW, {now_price}')
-
-    generate_mayer_values(BTC_price_data_csv, BTC_mayer_values_csv)
-    generate_day_ratios(BTC_price_data_csv, BTC_day_ratios_csv)
-
-    write_data_to_worksheet(BTC_mayer_values_csv, "BTC Mayer Multiples", yesterday)
-    write_data_to_worksheet(BTC_day_ratios_csv, "BTC Day Ratios", yesterday)
-
-
-    # ETH below
-
-    print()
-    price_data_from_csv = import_csv_as_list(ETH_price_data_csv)
-    most_recent_date_in_price_data = price_data_from_csv[-1][0]
-
-    yesterday = get_yesterday()
-
-    if most_recent_date_in_price_data == "NOW":
-        most_recent_date_in_price_data = price_data_from_csv[-2][0]
-        remove_last_row_from_csv(ETH_price_data_csv)
-
-    if most_recent_date_in_price_data < yesterday:
-
-        print(f'Most recent date in "{ETH_price_data_csv}": {most_recent_date_in_price_data}')
-        print('Gathering missing price data...\n')
-
-        missing_price_dates = get_date_range(get_day_after(most_recent_date_in_price_data), yesterday)
-
-        missing_price_data = get_price_dict_for_dates(ETH_currency_pair, missing_price_dates)
-
-        append_data_to_csv(ETH_price_data_csv, missing_price_data)
-        print(f'Updated "{ETH_price_data_csv}" with {len(missing_price_data)} record(s).\n')
-
-    else:
-        print(f'Most recent date in "{ETH_price_data_csv}" ({most_recent_date_in_price_data}) ' + \
-              f'is not before yesterday ({yesterday}); no data are missing.\n')
-
-    now_price = coinbase_client.get_spot_price(currency_pair=ETH_currency_pair)['amount']
-    print('ETH current: ${:,.2f}\n'.format(float(now_price)))
-    with open(ETH_price_data_csv,'a') as fd:
-        fd.write(f'NOW, {now_price}')
-
-    generate_mayer_values(ETH_price_data_csv, ETH_mayer_values_csv)
-    generate_day_ratios(ETH_price_data_csv, ETH_day_ratios_csv)
-
-    write_data_to_worksheet(ETH_mayer_values_csv, "ETH Mayer Multiples", yesterday)
-    write_data_to_worksheet(ETH_day_ratios_csv, "ETH Day Ratios", yesterday)
+    for i in range(len(coins_info)):
+        update_price_data(coins_info[i][0], coins_info[i][3], yesterday)
+        generate_mayer_values(coins_info[i][0], coins_info[i][1])
+        generate_day_ratios(coins_info[i][0], coins_info[i][2])
+        write_data_to_worksheet(coins_info[i][1], coins_info[i][4], yesterday)
+        write_data_to_worksheet(coins_info[i][2], coins_info[i][5], yesterday)
--- config.py
+++ config.py
@@ -8,12 +8,11 @@ mayer_ranges = []
 day_ratio_ranges = []
 [day_ratio_ranges.append(i) for i in range(1, 401)]
 
-BTC_price_data_csv = 'BTC_price_data.csv'
-BTC_mayer_values_csv = 'output-data/BTC_mayer_values.csv'
-BTC_day_ratios_csv = 'output-data/BTC_day_ratios.csv'
-BTC_currency_pair = 'BTC-USD'
-
-ETH_price_data_csv = 'ETH_price_data.csv'
-ETH_mayer_values_csv = 'output-data/ETH_mayer_values.csv'
-ETH_day_ratios_csv = 'output-data/ETH_day_ratios.csv'
-ETH_currency_pair = 'ETH-USD'
+coins_info = [
+    ['BTC_price_data.csv', 'output-data/BTC_mayer_values.csv',\
+     'output-data/BTC_day_ratios.csv', 'BTC-USD',\
+     'BTC Mayer Multiples', 'BTC Day Ratios'],
+    ['ETH_price_data.csv', 'output-data/ETH_mayer_values.csv',\
+     'output-data/ETH_day_ratios.csv', 'ETH-USD',\
+     'ETH Mayer Multiples', 'ETH Day Ratios']\
+]
