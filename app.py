e3578aadfbc214afa2c2329781674a7e411c960b
Jed Duffey <jedduffey@gmail.com>
2020-07-01 22:41:59 -0400

    move remaining functions to utils file

--- app.py
+++ app.py
@@ -1,76 +1,11 @@
-import csv
-import pandas as pd
-from coinbase.wallet.client import Client
-from utils import generate_mayer_values, generate_day_ratios,\
-                  write_data_to_worksheet, get_yesterday,\
-                  get_date_range, get_day_after
 from config import coin_vars
 import printer
-
-coinbase_client = Client('<KEY_NOT_NEEDED>', '<SECRET_NOT_NEEDED>')
-
-
-def import_csv_as_list(csv_filename):
-    with open(csv_filename, 'r') as f:
-        return list(csv.reader(f))
-
-
-def get_most_recent_date(csv_filename):
-    with open(csv_filename, 'r') as f:
-        return list(csv.reader(f))[-1][0]
-
-
-def get_spot_price_for_date(currency_pair, date):
-    price_data = coinbase_client.get_spot_price(currency_pair=currency_pair, date=date)
-    return price_data['amount']
-
-
-def get_price_dict_for_dates(currency_pair, dates):
-    dates_and_prices = {}
-    for date in dates:
-        price = get_spot_price_for_date(currency_pair, date)
-        dates_and_prices[date] = price
-        printer.date_and_price(date, price)
-
-    return dates_and_prices
-
-
-def append_data_to_csv(csv_filename, data):
-    with open(csv_filename, 'a') as f:
-        for key in data.keys():
-            f.write("%s,%s\n"%(key, data[key]))
-
-
-def remove_last_row_from_csv(csv_filename):
-    df = pd.read_csv(csv_filename, skiprows=0)
-    df.drop(df.tail(1).index,inplace=True)
-    df.to_csv(csv_filename, index=False)
-
-
-def update_price_data(price_data_csv, currency_pair, yesterday):
-    price_data_from_csv = import_csv_as_list(price_data_csv)
-    most_recent_date_in_price_data = price_data_from_csv[-1][0]
-    if most_recent_date_in_price_data == "NOW":
-        most_recent_date_in_price_data = price_data_from_csv[-2][0]
-        remove_last_row_from_csv(price_data_csv)
-    if most_recent_date_in_price_data < yesterday:
-        printer.gathering_missing_price_data(price_data_csv, most_recent_date_in_price_data)
-        missing_price_dates = get_date_range(get_day_after(most_recent_date_in_price_data), yesterday)
-        missing_price_data = get_price_dict_for_dates(currency_pair, missing_price_dates)
-        append_data_to_csv(price_data_csv, missing_price_data)
-        printer.updated_price_data(price_data_csv, missing_price_data)
-    else:
-        printer.no_data_missing_from_price_data(price_data_csv, most_recent_date_in_price_data, yesterday)
-
-    now_price = coinbase_client.get_spot_price(currency_pair=currency_pair)['amount']
-    printer.current_price(coin, now_price)
-    with open(price_data_csv,'a') as fd:
-        fd.write(f'NOW, {now_price}')
+import utils
 
 
 if __name__ == '__main__':
 
-    yesterday = get_yesterday()
+    yesterday = utils.get_yesterday()
 
     coins = ['BTC', 'ETH']
     for coin in coins:
@@ -82,10 +17,10 @@ if __name__ == '__main__':
         day_ratios = coin_vars[coin]['day_ratios']
 
         try:
-            update_price_data(price_data, currency_pair, yesterday)
-            generate_mayer_values(price_data, mayer_values)
-            generate_day_ratios(price_data, day_ratios)
-            write_data_to_worksheet(mayer_values, coin_vars[coin]['gsheet_mayer_values'], yesterday)
-            write_data_to_worksheet(day_ratios, coin_vars[coin]['gsheet_day_ratios'], yesterday)
+            utils.update_price_data(price_data, coin, currency_pair, yesterday)
+            utils.generate_mayer_values(price_data, mayer_values)
+            utils.generate_day_ratios(price_data, day_ratios)
+            utils.write_data_to_worksheet(mayer_values, coin_vars[coin]['gsheet_mayer_values'], yesterday)
+            utils.write_data_to_worksheet(day_ratios, coin_vars[coin]['gsheet_day_ratios'], yesterday)
         except Exception as error:
             printer.exception_encountered(error, coin)
--- utils.py
+++ utils.py
@@ -1,21 +1,23 @@
+from coinbase.wallet.client import Client
 import csv
+from datetime import datetime, timedelta
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from os import path, mkdir
 import pandas as pd
 from time import sleep
+
 from config import mayer_ranges, day_ratio_ranges
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
-from datetime import datetime, timedelta
-from os import path, mkdir
 import printer
 
 
-# Google settings
 workbook_name = "Mayer Fields Data"
+google_client_secret_json = 'creds/client_secret.json'
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-google_client_secret_json = 'creds/client_secret.json'
 creds = ServiceAccountCredentials.from_json_keyfile_name(google_client_secret_json, scope)
 google_client = gspread.authorize(creds)
+coinbase_client = Client('<KEY_NOT_NEEDED>', '<SECRET_NOT_NEEDED>')
 
 
 def get_list_of_dates_from_gsheet(worksheet_name):
@@ -142,3 +144,55 @@ def write_data_to_worksheet(csv_filename, worksheet_name, yesterday):
 
         price_now_row = format_row(data[-1])
         worksheet.insert_row(price_now_row, first_empty_row_index)
+
+
+def get_most_recent_date(csv_filename):
+    with open(csv_filename, 'r') as f:
+        return list(csv.reader(f))[-1][0]
+
+
+def get_spot_price_for_date(coinbase_client, currency_pair, date):
+    price_data = coinbase_client.get_spot_price(currency_pair=currency_pair, date=date)
+    return price_data['amount']
+
+
+def get_price_dict_for_dates(currency_pair, dates):
+    dates_and_prices = {}
+    for date in dates:
+        price = get_spot_price_for_date(currency_pair, date)
+        dates_and_prices[date] = price
+        printer.date_and_price(date, price)
+    return dates_and_prices
+
+
+def append_data_to_csv(csv_filename, data):
+    with open(csv_filename, 'a') as f:
+        for key in data.keys():
+            f.write(f'{key},{data[key]}\n')
+
+
+def remove_last_row_from_csv(csv_filename):
+    df = pd.read_csv(csv_filename, skiprows=0)
+    df.drop(df.tail(1).index, inplace=True)
+    df.to_csv(csv_filename, index=False)
+
+
+def update_price_data(price_data_csv, coin, currency_pair, yesterday):
+    price_data_from_csv = import_csv_as_list(price_data_csv)
+    most_recent_date_in_price_data = price_data_from_csv[-1][0]
+    if most_recent_date_in_price_data == "NOW":
+        most_recent_date_in_price_data = price_data_from_csv[-2][0]
+        remove_last_row_from_csv(price_data_csv)
+    if most_recent_date_in_price_data < yesterday:
+        printer.gathering_missing_price_data(price_data_csv, most_recent_date_in_price_data)
+        missing_price_dates = get_date_range(get_day_after(most_recent_date_in_price_data), yesterday)
+        missing_price_data = get_price_dict_for_dates(currency_pair, missing_price_dates)
+        append_data_to_csv(price_data_csv, missing_price_data)
+        printer.updated_price_data(price_data_csv, missing_price_data)
+    else:
+        printer.no_data_missing_from_price_data(price_data_csv, most_recent_date_in_price_data, yesterday)
+
+    now_price = coinbase_client.get_spot_price(currency_pair=currency_pair)['amount']
+    printer.current_price(coin, now_price)
+    with open(price_data_csv, 'a') as fd:
+        fd.write(f'NOW,{now_price}')
