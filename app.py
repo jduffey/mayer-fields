ad94416df14c9bb45510b1e80f3892a55e1c612f
Jed Duffey <jedduffey@gmail.com>
2020-07-27 23:59:50 -0400

    find price where SMA200 > 2.4 (needs refactored)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from config import coin_vars
 import printer
 import utils
+import pandas as pd
 
 
 if __name__ == '__main__':
@@ -24,3 +25,27 @@ if __name__ == '__main__':
             utils.write_data_to_worksheet(day_ratios, coin_vars[coin]['gsheet_day_ratios'], yesterday)
         except Exception as error:
             printer.exception_encountered(error, coin)
+
+
+    def find_mayer_price():
+        now_date = 'NOW'
+        now_price = 0
+        sma200_value = 0
+        target_sma_ratio = 2.4
+        price_jump = 100
+
+        while sma200_value < target_sma_ratio:
+            csv_filename = 'price-data/BTC_price_data.csv'
+
+            df = pd.read_csv(csv_filename, skiprows=0, skipfooter=1)
+            df.loc[df.index.max() + 1] = [now_date, now_price]
+
+            sma200_values = df['Spot']/df['Spot'].rolling(window=(200 + 1)).mean()
+
+            sma200_value = sma200_values[sma200_values.index.max()]
+
+            print(f'${now_price}: {sma200_value}')
+
+            now_price = now_price + price_jump
+
+    find_mayer_price()
