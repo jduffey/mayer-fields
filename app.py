16925a32a6479618ce11295fa254d20364a3ded5
Jed Duffey <jedduffey@gmail.com>
2020-07-28 00:11:59 -0400

    print SMA200 > 2.4 prices for both coins

--- app.py
+++ app.py
@@ -27,15 +27,16 @@ if __name__ == '__main__':
             printer.exception_encountered(error, coin)
 
 
-    def find_mayer_price():
+    target_sma_ratio = 2.4
+    def find_mayer_price(coin):
+        coin_name = coin[0]
+        coin_jump = coin[1]
         now_date = 'NOW'
         now_price = 0
         sma200_value = 0
-        target_sma_ratio = 2.4
-        price_jump = 100
 
         while sma200_value < target_sma_ratio:
-            csv_filename = 'price-data/BTC_price_data.csv'
+            csv_filename = f'price-data/{coin_name}_price_data.csv'
 
             df = pd.read_csv(csv_filename, skiprows=0, skipfooter=1)
             df.loc[df.index.max() + 1] = [now_date, now_price]
@@ -44,8 +45,11 @@ if __name__ == '__main__':
 
             sma200_value = sma200_values[sma200_values.index.max()]
 
-            print(f'${now_price}: {sma200_value}')
+            now_price = now_price + coin_jump
 
-            now_price = now_price + price_jump
+        print(f'{coin_name}: ${now_price}: {sma200_value}')
 
-    find_mayer_price()
+    coins = [('BTC', 50), ('ETH', 5)]
+    print(f'=== Price where SMA200 > {target_sma_ratio} ===')
+    for coin in coins:
+        find_mayer_price(coin)
