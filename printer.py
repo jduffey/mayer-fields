1ec21c98b4f8b73a5e625212e7b74964a60d01cb
Jed Duffey <jedduffey@gmail.com>
2020-08-01 22:08:16 -0400

    minor edit on hint_vpn()

--- printer.py
+++ printer.py
@@ -71,4 +71,4 @@ def writing_data_to_worksheet(worksheet_name):
     print(f'Writing missing data to "{worksheet_name}"...')
 
 def hint_vpn():
-    print(f'HINT: Are you on a VPN or attempting to connect from a deny-listed IP?')
+    print(f'HINT: Are you on a VPN or attempting to connect from a deny-listed IP address?')
