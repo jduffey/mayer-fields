cf549c705bcc227dd10ce9ac6a82c41ca9333088
Jed Duffey <jedduffey@gmail.com>
2020-08-08 18:31:40 -0400

    changed mind; pass in currency_pair instead of coin in coinbase_utils methods

--- app.py
+++ app.py
@@ -14,13 +14,14 @@ if __name__ == '__main__':
     for coin in coins:
         printer.initiating_workflow(coin)
 
+        currency_pair = f'{coin}-USD'
         price_data = coin_vars[coin]['price_data']
         mayer_values = coin_vars[coin]['mayer_values']
         day_ratios = coin_vars[coin]['day_ratios']
 
         try:
-            now_price = coinbase_utils.get_current_price(coin)
-            coinbase_utils.update_price_data(price_data, coin, yesterday, now_price)
+            now_price = coinbase_utils.get_current_price(currency_pair)
+            coinbase_utils.update_price_data(price_data, currency_pair, yesterday, now_price)
             utils.generate_mayer_values(price_data, mayer_values)
             utils.generate_day_ratios(price_data, day_ratios)
             utils.write_data_to_worksheet(mayer_values, coin_vars[coin]['gsheet_mayer_values'], yesterday)
