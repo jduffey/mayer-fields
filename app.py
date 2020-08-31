2870cf9aae1e6dbf641bf0c823d6b92e6850eff4
Jed Duffey <jedduffey@gmail.com>
2020-08-30 22:59:01 -0400

    refactor app... ugly method name :-(

--- app.py
+++ app.py
@@ -7,11 +7,19 @@ import coinbase_utils
 import google_utils
 
 
+def update_target_mayer_value_price_and_updated_dashboard(coin):
+    utils.find_mayer_prices(coin)
+    google_utils.write_time_updated(coin[0])
+
+
 if __name__ == '__main__':
 
+    target_sma_ratio_min = 2.4
+    target_sma_ratio_step = 0.1
+    target_sma_ratios = [target_sma_ratio_min + target_sma_ratio_step * x for x in range(11)]
     yesterday = utils.get_yesterday()
-    coins = [('BTC', 5), ('ETH', 1)]
 
+    coins = [('BTC', 5), ('ETH', 1)]
     for coin in coins:
         coin_name = coin[0]
         printer.initiating_workflow(coin_name)
@@ -28,14 +36,7 @@ if __name__ == '__main__':
             utils.generate_day_ratios(price_data, day_ratios)
             google_utils.write_data_to_worksheet(mayer_values, coin_vars[coin_name]['gsheet_mayer_values'], yesterday)
             google_utils.write_data_to_worksheet(day_ratios, coin_vars[coin_name]['gsheet_day_ratios'], yesterday)
+            update_target_mayer_value_price_and_updated_dashboard(coin)
         except Exception as error:
             printer.exception_encountered(error, coin_name)
             printer.hint_vpn()
-
-    target_sma_ratio_min = 2.4
-    target_sma_ratio_step = 0.1
-    target_sma_ratios = [target_sma_ratio_min + target_sma_ratio_step * x for x in range(11)]
-
-    for coin in coins:
-        utils.find_mayer_prices(coin)
-        google_utils.write_time_updated(coin[0])
